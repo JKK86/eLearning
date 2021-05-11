766a9f8b3d792e06d002ec5b79243f4f9bda20f1
JKK86 <j.kurowski86@wp.pl>
2021-05-11 20:36:10 +0200

    create owner mixins

--- courses/models.py
+++ courses/models.py
@@ -23,7 +23,7 @@ class Course(models.Model):
     name = models.CharField(max_length=255, verbose_name='Nazwa kursu')
     slug = models.SlugField(max_length=255, unique=True)
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name='courses', verbose_name='Kategorie')
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='courses_created')
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='courses_created')
     overview = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
