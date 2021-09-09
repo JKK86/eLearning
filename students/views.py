59a761eceb1ee65c5b2fdb3c674b3f603bd24460
JKK86 <j.kurowski86@wp.pl>
2021-09-10 01:15:03 +0200

    add students to course view and fix registration view

--- students/views.py
+++ students/views.py
@@ -1,13 +1,16 @@
+from braces.views import LoginRequiredMixin
 from django.contrib.auth import authenticate, login
-from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, FormView
+
+from students.forms import CourseEnrollForm
+from users.forms import CustomUserCreationForm
 
 
 class StudentsRegistrationView(CreateView):
     template_name = 'students/student/registration.html'
-    form_class = UserCreationForm
+    form_class = CustomUserCreationForm
     success_url = reverse_lazy('student_course_list')
 
     def form_valid(self, form):
@@ -16,3 +19,16 @@ class StudentsRegistrationView(CreateView):
                             password=form.cleaned_data['password'])
         login(self.request, user)
         return result
+
+
+class StudentEnrollCourseView(LoginRequiredMixin, FormView):
+    course = None
+    form_class = CourseEnrollForm
+
+    def form_valid(self, form):
+        self.course = form.cleaned_data['course']
+        self.course.students.add(self.request.user)
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('student_course_detail', args=[self.course.id])
