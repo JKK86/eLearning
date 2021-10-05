7089c1e5ae24bb9e4bc2a595d00fb9cbd722b93f
JKK86 <j.kurowski86@wp.pl>
2021-10-05 11:32:58 +0200

    add subject api views

--- eLearning/urls.py
+++ eLearning/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('course/', include('courses.urls')),
     path('account/', include('users.urls')),
     path('students/', include('students.urls')),
+    path('api/', include('courses.api.urls', namespace='api')),
 
     path('', CourseListView.as_view(), name="course_list"),
 ]
